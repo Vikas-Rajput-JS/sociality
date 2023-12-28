402f566ae16e84c83fbc578ee3d635ffd4ca3069
JC25 <vikashrajput@jcsoftwaresolution.com>
2023-12-28 13:09:15 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-  
+
+
   <React.StrictMode>
     <App />
   </React.StrictMode>
+
   </BrowserRouter>
 );
 
