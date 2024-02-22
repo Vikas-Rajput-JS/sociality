a203a5d766099d471f535ca1f883c959e94a2879
Vikas Rajput <39953644+Vikas-Rajput-JS@users.noreply.github.com>
2024-02-22 22:55:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4687;
 
 // Serve static files from the build folder
 app.use(express.static(path.join(__dirname, 'build')));
